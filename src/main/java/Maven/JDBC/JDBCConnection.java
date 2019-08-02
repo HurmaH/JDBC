90c944b1f54a4206970a9cb8442baab3b7464a5d
Hurma H <hurmahm@gmail.com>
2019-08-02 00:25:24 -0500

    Metadata

--- src/main/java/Maven/JDBC/JDBCConnection.java
+++ src/main/java/Maven/JDBC/JDBCConnection.java
@@ -2,13 +2,28 @@ package Maven.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.testng.annotations.Test;
 
+//Resultset methods
+		/*
+		 * 1. next()                 -> moves to next row
+		 * 2.getObject(colName/index)-> read data from coumn
+		 * 3.last()                  -> goes to last row
+		 * 4.previous();             -> goes to previous row
+		 * 5.first();                -> goes to first row
+		 * 6.beforeFirst();
+		 * 7.afterLast()
+		 * 8.getRow()                -> returns int of the row
+		 * 9.absolute(row)           -> jums to specific row
+		 */
 public class JDBCConnection {
-	String oracleDbURL="jdbc:mysql://hostname:3306/qadbt?autoReconnect=true&useSSL=false";
-	String oraclDbUsername="root";
-	String oracleDbPassword="1234";
+	String oracleDbURL="jdbc:oracle:thin:@database:1521:xe";
+	String oraclDbUsername="hr";
+	String oracleDbPassword="hr";
 	
 	@Test
 	public void jdbcTest() throws SQLException {
@@ -16,7 +31,45 @@ public class JDBCConnection {
 		Connection connection = DriverManager.getConnection(oracleDbURL,
 				                                            oraclDbUsername,
 				                                            oracleDbPassword);
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from countries");
+		
+		System.out.println(resultSet.getRow());//will return zero as it is outside
+		resultSet.next();//will go inside the table
+		System.out.println(resultSet.getRow());//will return 1 as it is inside
+		
+		//resultSet.previous();
+		//resultSet.first();
+		//resultSet.last();
+		//resultSet.getRow();//-> will give error as it is going only forward and works with next()
+		
+		resultSet.next();//will go to next line
+		System.out.println(resultSet.getString("country_name"));//country_name is column name, will read first row of this column, value is String
+		System.out.println(resultSet.getInt("region_id"));//region_id is column name, will read first row of this column, value is int
+		
+		
+		//find out how many records in resultSet
+		resultSet.last();
+		int rowsCount=resultSet.getRow();
+		System.out.println("Number of rows:"+rowsCount);
+		
+		//Iterating
+		resultSet.beforeFirst();//reslutSet.fist(); could be used but first value would be missed inside the while loop
+		while (resultSet.next()) {//resultSet.next() will go to next row first then return boolean value:true/false
+			
+			System.out.println(resultSet.getString(1)+"-"+resultSet.getString("country_name")+"-"+resultSet.getInt("country_id"));
+		}
+		
+		
+		
+		
+		System.out.println();
+		
+		System.out.println();
 	
+		statement.close();
+		resultSet.close();
 		connection.close();
 	}
 
--- src/main/java/Maven/JDBC/Metadata.java
+++ src/main/java/Maven/JDBC/Metadata.java
@@ -1,5 +1,54 @@
 package Maven.JDBC;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
 public class Metadata {
+	String oracleDbURL = "jdbc:oracle:thin:@database:1521:xe";
+	String oraclDbUsername = "hr";
+	String oracleDbPassword = "hr";
+
+	@Test
+	public void jdbcMetadata() throws Exception {
+		Connection connection = DriverManager.getConnection(oracleDbURL, oraclDbUsername, oracleDbPassword);
+		Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CLOSE_CURSORS_AT_COMMIT);
+
+		//String sql = "select employee_id, last_name, job_id, salary from employees";
+		String sql = "select * from employees";
+		ResultSet resultSet = statement.executeQuery(sql);
+		
+		//DataBase metadata
+		DatabaseMetaData dbMetadata = connection.getMetaData();
+		System.out.println("User:"+dbMetadata.getUserName());
+		System.out.println("Database type:"+dbMetadata.getDatabaseProductName());
+		
+		//resultSet metadata
+		ResultSetMetaData rsMetadata = resultSet.getMetaData();
+		System.out.println("Columns count:"+rsMetadata.getColumnCount());
+		System.out.println(rsMetadata.getColumnName(1));
+		
+		//print all column names:
+		for (int i=1; i<=rsMetadata.getColumnCount(); i++ ) {
+			System.out.println(i+" -> "+rsMetadata.getColumnName(i));
+		}
+		
+		//Throw resultSet into a List of Maps
+		//1.Create List of Maps
+		List <Map <String, Object>> list = new ArrayList<>();
+		
+		statement.close();
+		resultSet.close();
+		connection.close();
+	}
 
 }
